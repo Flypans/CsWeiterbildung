d3bb46abb849817f3513bf869c9a4c7c32934f97
Flypans <sbkwon011@outlook.com>
2024-04-25 14:03:43 +0200

    add Routing Strategien

---
+++ CSH04/CSH04_Lektion4_3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CSH04_Lektion4_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Mouse_Event(object sender, MouseButtonEventArgs e)
+        {
+            listBox.Items.Add("B: " + sender.ToString());
+            //e.Handled = true;
+        }
+
+        private void PreviewMouseEvent(object sender, MouseButtonEventArgs e)
+        {
+            listBox.Items.Add("T: " + sender.ToString());
+            //e.Handled = true;
+        }
+    }
+}
