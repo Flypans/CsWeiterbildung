814729e3a8a91d29d9f949741170dee6e4376277
Flypans <sbkwon011@outlook.com>
2024-05-06 14:11:05 +0200

    add Laden and Flugauswahl Dialog

---
+++ CSH05/CSH05_Fliegerprojekt/Flugauswahldialog.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Db4objects.Db4o;
+
+namespace CSH05_Fliegerprojekt
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    /// 
+    partial class Flugauswahldialog : Window
+    {
+        private string dbName;
+
+        public Duesenflugzeug Flugauswahl
+        {
+            get
+            {
+                return (Duesenflugzeug)cBoxFluege.SelectedItem;
+            }
+        }
+
+        public Flugauswahldialog(string dbName)
+        {
+            this.dbName = dbName;
+
+            InitializeComponent();
+            Flugauswahldialog_Load();
+        }
+
+        private void Flugauswahldialog_Load()
+        {
+            IObjectContainer db = null;
+
+
+
+            try
+            {
+                db = Db4oFactory.OpenFile(dbName);
+                IObjectSet result = db.QueryByExample(typeof(Duesenflugzeug));
+
+                while (result.HasNext())
+                {
+                    cBoxFluege.Items.Add(result.Next());
+                    cBoxFluege.SelectedIndex = 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType() + ": " + ex.Message);
+            }
+            finally
+            {
+                if (db != null)
+                {
+                    db.Close();
+                }
+            }
+        }
+
+        private void Abbrechen_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- CSH05/CSH05_Fliegerprojekt/Konfigurationsdialog.xaml.cs
+++ CSH05/CSH05_Fliegerprojekt/Konfigurationsdialog.xaml.cs
@@ -26,9 +26,9 @@ namespace CSH05_Fliegerprojekt
 
         public Konfigurationsdialog(Duesenflugzeug flieger)
         {
-            this.flieger = flieger;
+            this.flieger = flieger; //member variable
 
-            InitializeComponent();
+            InitializeComponent(); //WPF 
             Konfigurationsdialog_Load();
         }
 
@@ -44,7 +44,7 @@ namespace CSH05_Fliegerprojekt
 
         private void initializeFlieger()
         {
-            flieger.Kennung = tbKennung.Text;
+            flieger.Kennung = tbKennung.Text; // call by reference
 
             if (flieger.Kennung.Length == 0)
             {
@@ -67,13 +67,32 @@ namespace CSH05_Fliegerprojekt
             }
         }
 
-        private void SetEingabewerte()
+        public void SetEingabewerte(Duesenflugzeug flieger)
         {
             //textBoxKennung.Text = "LH 500";
             //textBoxStartPosX.Text = "100";
 
-            tbKennung.Text = "LH 500";
-            tbKennung.Text = "100";
+            //tbKennung.Text = "LH 500";
+            //tbKennung.Text = "100";
+
+            tbKennung.Text = flieger.Kennung;
+            cbTyp.SelectedItem = flieger.typ;
+
+            tbStartX.Text = flieger.pos.x.ToString();
+            tbStartY.Text = flieger.pos.y.ToString();
+            tbStartH.Text = flieger.pos.h.ToString();
+
+            tbZielX.Text = flieger.zielPos.x.ToString();
+            tbZielY.Text = flieger.zielPos.y.ToString();
+            tbZielH.Text = flieger.zielPos.h.ToString();
+
+            tbFlughoehe.Text = flieger.flughoehe.ToString();
+
+            tbSteighoehe.Text = flieger.steighoeheProTakt.ToString();
+            tbSinkhoehe.Text = flieger.sinkhoeheProTakt.ToString();
+
+            tbFlugstrecke.Text = flieger.streckeProTakt.ToString();
+            tbAnzahlPlaetze.Text = flieger.sitzplaetze.ToString();
         }
         private void beenden_Click(object sender, RoutedEventArgs e)
         {
@@ -82,39 +101,38 @@ namespace CSH05_Fliegerprojekt
         private string dbName = "FliegerDB";
         private void buttonSpeichern_Click(object sender, RoutedEventArgs e)
         {
-            IObjectContainer db = null;
+            IObjectContainer db = null; // declare and initialize the variable db
 
-            //DB-Update-Arbeitsobjekts
-            bool update = false; 
+            //This variable is used to track whether an update operation is needed in the database. 
+            bool update = false;
 
             try
             {
                 db = Db4oFactory.OpenFile(dbName);
 
                 //DB-Update-Arbeitsobjekts [
-                IList<Duesenflugzeug> fluege = db.Query<Duesenflugzeug>( 
-                    delegate (Duesenflugzeug flieger) 
-                    { 
-                        return flieger.Kennung == tbKennung.Text; 
+                IList<Duesenflugzeug> fluege = db.Query<Duesenflugzeug>(
+                    delegate (Duesenflugzeug flieger)
+                    {
+                        return flieger.Kennung == tbKennung.Text;
                     });
 
-                foreach(Duesenflugzeug flieger in fluege)
+                foreach (Duesenflugzeug flieger in fluege)
                 {
                     Console.WriteLine("Flug mit der Kennung {0} in Datenbank gefunden", flieger.Kennung);
                 }
 
-                if(fluege.Count > 0)
+                if (fluege.Count > 0)
                 {
                     flieger = fluege.First();
                     update = true;
                 }
                 //DB-Update-Arbeitsobjekts ]
-
                 this.initializeFlieger();
                 db.Store(flieger);
 
                 //DB-Update-Arbeitsobjekts [
-                if(update)
+                if (update)
                 {
                     Console.WriteLine("Datenbank-Update für den Flug mit der kennung {0}", flieger.Kennung);
                 }
@@ -134,6 +152,28 @@ namespace CSH05_Fliegerprojekt
                 {
                     db.Close();
                 }
+            }
+        }
+        private void buttonLaden_Click(object sender, EventArgs e)
+        {
+            //Flugauswahldialog flugauswahlDialog = new Flugauswahldialog(dbName);
+            //flugauswahlDialog.ShowDialog();
+
+            Flugauswahldialog select = new Flugauswahldialog(dbName);
+            select.Height = 200;
+            select.Width = 400;
+
+            //if (result == DialogResult.OK)
+            if(select.ShowDialog() == true)
+            {
+                flieger = select.Flugauswahl;
+                this.SetEingabewerte(flieger);
+
+                Console.WriteLine("Flug \"{0}\" geladen", flieger.Kennung);
+            }
+            else
+            {
+                Console.WriteLine("Flugauswahl abgebrochen");
             }
         }
     }
